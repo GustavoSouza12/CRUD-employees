646dd52c6b82457b6025a1551317a1d6a1cdf216
Gustavo Souza <GustavoSouzaAlves12@gmail.com>
2020-10-05 22:19:32 -0300

    Connection with MySQL established

--- server/index.js
+++ server/index.js
@@ -1,10 +1,24 @@
 const express = require('express')
 const app = express()
+const mysql = require('mysql')
 
-app.get("/",(req, res) =>{
-    res.send("")
+const db = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: 'password',
+    database: 'register_form'
 })
 
-app.listen(3001), () =>{
+app.get("/",(req, res) => {
+
+    const sqlInsert = "INSERT INTO register_new_employeer (form_name, form_lastName, form_dateOfBirth, form_salary, form_office, form_officeDescription) VALUES ('gustavo', 'silva', '31/05/2001','3,000','programador', 'html,css,js');"
+
+    db.query(sqlInsert, (err, result)=>{
+        res.send("OIIIIIII") 
+    })
+  
+})
+
+app.listen(3001), () => {
     console.log('running on port 3001')
 }
