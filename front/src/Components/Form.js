59c6501e0f6b3c15442c4ea0b615b87dc8f62f70
Gustavo Souza <GustavoSouzaAlves12@gmail.com>
2020-10-06 00:43:22 -0300

    Front to back and MySQL conection established

--- front/src/Components/Form.js
+++ front/src/Components/Form.js
@@ -1,7 +1,27 @@
-import React from 'react';
-import './Form.css';
-
+import React, { useState, useEffect } from 'react'
+import './Form.css'
+import axios from 'axios';
 function Form() {
+
+  const [formName, setFormName] = useState('')
+  const [formLastName, setFormLastName] = useState('')
+  const [formDateOfBirth, setFormDateOfBirth] = useState('')
+  const [formSalary, setFormSalary] = useState('')
+  const [formOffice, setFormOffice] = useState('')
+  const [formOfficeDescription, setFormOfficeDescription] = useState('')
+
+  const submitData = () =>{
+    axios.post('https/localhost:3001/api/insert', 
+    {formName: formName,                                   
+     formLastName: formLastName,                                
+     formDateOfBirth: formDateOfBirth,                                
+     formSalary: formSalary,                                
+     formOffice: formOffice,                                  
+     formOfficeDescription: formOfficeDescription
+    }).then(()=>{
+      alert('sucesssssss')
+    })
+  }
   return (
     <div className="App">
       <form className="form">
@@ -9,24 +29,36 @@ function Form() {
         <h2 className="form_title">Cadastrar Funcionário</h2>
        
         <label>Nome:</label>
-        <input type="text" name="name"/>
+        <input type="text" name="name" onChange={(e)=>{
+          setFormName(e.target.value)
+        }}/>
    
         <label>Último nome:</label>
-        <input type="text" name="lastName"/>
+        <input type="text" name="lastName"onChange={(e)=>{
+          setFormLastName(e.target.value)
+        }}/>
        
         <label>Data de nascimento:</label>
-        <input type="text" name="dateOfBirth"/>
+        <input type="text" name="dateOfBirth"onChange={(e)=>{
+          setFormDateOfBirth(e.target.value)
+        }}/>
     
         <label>Salário</label>
-        <input type="text" name="salary"/>
+        <input type="text" name="salary"onChange={(e)=>{
+          setFormSalary(e.target.value)
+        }}/>
       
         <label>Cargo</label>
-        <input type="text" name="office"/>
+        <input type="text" name="office"onChange={(e)=>{
+          setFormOffice(e.target.value)
+        }}/>
     
         <label>Descrição do cargo</label>
-        <input type="text" className="form_officeDescription" name="officeDescription"/>
+        <input type="text" className="form_officeDescription" name="officeDescription"onChange={(e)=>{
+          setFormOfficeDescription(e.target.value)
+        }}/>
     
-        <button className="form_button">Cadastrar</button>
+        <button className="form_button" onClick={submitData}>Cadastrar</button>
       </form>  
     </div>
   );
--- server/index.js
+++ server/index.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
 const app = express()
 const mysql = require('mysql')
 
@@ -9,14 +11,30 @@ const db = mysql.createPool({
     database: 'register_form'
 })
 
-app.get("/",(req, res) => {
+app.use(cors())
+app.use(express.json())
+app.use(bodyParser.urlencoded({extended: true}))
 
-    const sqlInsert = "INSERT INTO register_new_employeer (form_name, form_lastName, form_dateOfBirth, form_salary, form_office, form_officeDescription) VALUES ('gustavo', 'silva', '31/05/2001','3,000','programador', 'html,css,js');"
+app.post("/api/insert",(req, res) => {
 
-    db.query(sqlInsert, (err, result)=>{
-        res.send("OIIIIIII") 
-    })
-  
+    const formName = req.body.formName
+    const formLastName = req.body.formLastName
+    const formDateOfBirth = req.body.formName
+    const formSalary = req.body.formSalary
+    const formOffice = req.body.formOffice
+    const formOfficeDescription = req.body.formOfficeDescription
+    
+    const sqlInsert = "INSERT INTO register_form (formName) VALUES ("
+    db.query(sqlInsert,
+        [formName, 
+        formLastName,
+        formDateOfBirth, 
+        formSalary, 
+        formOffice, 
+        formOfficeDescription], 
+        (err, result) =>{
+            console.log(result)
+        })
 })
 
 app.listen(3001), () => {
